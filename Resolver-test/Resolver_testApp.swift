96d12d219516235ce5393e509e9413c3a7acb7c9
James Froggatt <james.froggatt@experian.com>
2021-12-09 13:03:47 +0000

    Initial Commit

---
+++ Resolver-test/Resolver_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resolver_testApp.swift
+//  Resolver-test
+//
+//  Created by Froggatt, James on 09/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Resolver_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
